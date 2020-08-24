4dae76f83d938b1025ec6824e467d38a57639631
Rakha Djokosoetono <rakhadjo@gmail.com>
2020-08-24 13:05:16 +0700

    change RC.java -> .enum

--- src/main/java/com/example/spera_2/utils_config/RC.java
+++ src/main/java/com/example/spera_2/utils_config/RC.java
@@ -9,41 +9,30 @@ package com.example.spera_2.utils_config;
  *
  * @author rakhadjo
  */
-public class RC {
+public enum RC {
     
-    private String CODE;
-    private String MESSAGE;
-    private String[] msgArr = 
-    {
-    "success",                              //00
-    "please fill in all the blanks",        //01
-    "troops request != \"get all \"",       //02
-    "",                                     //03
-    "",                                     //04
-    "",                                     //05
-    "",                                     //06
-    "",                                     //07
-    "",                                     //08
-    "",                                     //09
-    "data doesn't exist",                   //10
-    "invalid request format",               //11
-    "undefined error"                       //12                   
-    };
+    SUCCESS,
+    DNE,
+    INVALID_FMT,
+    UNDEFINED
     
-    public RC() {}
-    public RC(String CODE) {
-        this.CODE = CODE;
-        this.MESSAGE = msgArr[Integer.parseInt(CODE)];
-    }
-    public RC(String CODE, String MESSAGE) {
-        this.CODE = CODE;
-        this.MESSAGE = MESSAGE;
-    }
-    
-    public String getCODE() { return CODE; }
-    public void setCODE(String CODE) { this.CODE = CODE; }
-    
-    public String getMESSAGE() { return MESSAGE; }
-    public void setMESSAGE(String MESSAGE) { this.MESSAGE = MESSAGE; }
+//    private String CODE;
+//    private String MESSAGE;
+//    private String[] msgArr = 
+//    {
+//    "success",                              //00
+//    "please fill in all the blanks",        //01
+//    "troops request != \"get all \"",       //02
+//    "",                                     //03
+//    "",                                     //04
+//    "",                                     //05
+//    "",                                     //06
+//    "",                                     //07
+//    "",                                     //08
+//    "",                                     //09
+//    "data doesn't exist",                   //10
+//    "invalid request format",               //11
+//    "undefined error"                       //12                   
+//    };
     
 }
